97adc5ee3df0e3660ab2ab8726b839330f6f7bd2
Sherilyn Sanchez <aicsssanchez@gmail.com>
2026-01-15 22:00:16 +0800

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+// bootstrap/app.php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,9 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
     	web: __DIR__.'/../routes/web.php',
    	 commands: __DIR__.'/../routes/console.php',
     	health: '/up',
-   	 then: function () {
-   	Route::middleware('web')->group(base_path('routes/migrate.php'));
-   	 }
 	)
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
